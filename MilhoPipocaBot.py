458a546ab9206e4b916e0f5334657342ae4e3231
Tykows Yan <Tykows Yan>
2016-05-02 01:11:50 -0300

    d

--- MilhoPipocaBot.py
+++ MilhoPipocaBot.py
@@ -5,6 +5,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG
 )
+
 logger = logging.getLogger(__name__)
 
 def start(bot, update):
