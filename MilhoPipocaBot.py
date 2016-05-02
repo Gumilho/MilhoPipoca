8a552a49b20a670464a3233f105bef008d2cb22d
Tykows Yan <Tykows Yan>
2016-05-02 20:53:30 -0300

    new script

--- MilhoPipocaBot.py
+++ MilhoPipocaBot.py
@@ -11,24 +11,35 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='Welcome!')
     
-def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Help!')
-   
-def echo(bot, update):
-    bot.sendMessage(update.message.chat_id, text=update.message.text)
-    
+def change(bot, update):
+    f = open("channel.txt", 'r+')
+    data = f.readlines()
+    bot.sendMessage(update.message.chat_id, text='Current channel: %(data[0]) \nDo you wish to change?(Y/N)')
+    if(bot.getUpdates()[-1] == 'Y'):
+        bot.sendMessage(update.message.chat_id, text='Type the new channel:')
+        old_channel = data[0]
+        new_channel = bot.getUpdates()[-1]
+        f.write(new_channel)
+        bot.sendMessage(update.message.chat_id, text='changed from %(old_channel) to %(new_channel) successfully!')
+    f.close()
+     
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
     
 def main():
     updater = Updater("168755507:AAHOZALQfHZ7qasSD1W6_Z_vCjqp8D4evbI")
-    dp = updater.dispatcher
-    dp.addHandler(CommandHandler("start", start))
-    dp.addHandler(CommandHandler("helo", help))
-    dp.addHandler(CommandHandler([Filters.text], echo))
-    dp.addErrorHandler(error)
+    last_command = bot.getUpdates()[-1]
     updater.start_polling()
     updater.idle()
+    if last_command.message.text == '\start':
+        bot.sendMessage(last_command.message.chat_id, text="Welcome! :)")
+    else:
+        updates = [update for update in bot.getUpdates() if update.message.text != '/start' || update.message.text != '/change']
+        for update in updates:
+            message = update.message.text
+            user_id = update.message.message_id
+            chat_id = update.message.chat_id
+            
     
 if __name__ == '__main__':
     main()
